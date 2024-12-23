8d71f702da5196ed575f92e10fe76fede2d89191
kyohei.shibuya <shibuya01055@gmail.com>
2024-12-24 02:26:55 +0900

    feat: OAuth連携追加

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,4 +1,12 @@
+require 'net/http'
+require 'uri'
+require 'json'
+
 class SessionsController < ApplicationController
+  MY_TWEET_APP_CLIENT_ID = ENV['MY_TWEET_APP_CLIENT_ID']
+  MY_TWEET_APP_CLIENT_SECRET = ENV['MY_TWEET_APP_CLIENT_SECRET']
+  REDIRECT_URI = ENV['REDIRECT_URI']
+
   def new
   end
 
@@ -9,7 +17,6 @@ class SessionsController < ApplicationController
     errors << 'パスワードを入力してください' if params[:password].blank?
 
     if errors.any?
-      p 'きてる？'
       flash[:alert] = errors
       return render :new, status: :unprocessable_entity
     end
@@ -29,5 +36,32 @@ class SessionsController < ApplicationController
     session[:user_id] = nil
     flash[:notice] = 'ログアウトしました'
     redirect_to login_path
+  end
+
+  def my_tweet_app_oauth
+    auth_url = 'http://unifa-recruit-my-tweet-app.ap-northeast-1.elasticbeanstalk.com/oauth/authorize?' +
+    "response_type=code&" +
+    "client_id=#{MY_TWEET_APP_CLIENT_ID}&" +
+    "redirect_uri=#{REDIRECT_URI}&" +
+    "scope=write_tweet"
+    redirect_to(auth_url, allow_other_host: true)
+  end
+
+  def callback
+    code = params[:code]
+    uri = URI('http://unifa-recruit-my-tweet-app.ap-northeast-1.elasticbeanstalk.com/oauth/token')
+    response = Net::HTTP.post_form(uri, {
+      code: code,
+      client_id: MY_TWEET_APP_CLIENT_ID,
+      client_secret: MY_TWEET_APP_CLIENT_SECRET,
+      redirect_uri: REDIRECT_URI,
+      grant_type: 'authorization_code'
+    })
+    token_data = JSON.parse(response.body)
+    access_token = token_data['access_token']
+    # sessionに保持
+    session[:access_token] = access_token
+
+    redirect_to root_path, notice: 'MyTweetAppと連携しました'
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,4 +6,7 @@ Rails.application.routes.draw do
   delete 'logout', to: 'sessions#destroy'
 
   resources :photos, only: %i(index new create)
+
+  get 'oauth/my_tweet_app', to: 'sessions#my_tweet_app_oauth', as: :my_tweet_app_oauth
+  get 'oauth/callback', to: 'sessions#callback', as: :oauth_callback
 end
